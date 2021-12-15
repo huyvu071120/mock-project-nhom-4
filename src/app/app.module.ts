aa0c8cd3273fc764f663628f526b74d162e760c7
Trần Huy Vũ <69894922+huyvu071120@users.noreply.github.com>
2021-12-15 14:51:27 +0700

    Create new component test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { NgxPaginationModule } from 'ngx-pagination';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [		
     AppComponent,
-      HomeComponent
+      HomeComponent,
+      TestComponent
    ],
   imports: [
     BrowserModule,
